a2767c496dd27a1ed432fe1b00b2c554f73d114c
Tom Six <tom.six@student.odisee.be>
2020-04-23 10:32:05 +0200

    initialize the laravel service provider

---
+++ src/ComponentServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace TomSix\Components;
+
+use Illuminate\Support\ServiceProvider;
+
+class ComponentServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+
+    }
+
+    public function register()
+    {
+
+    }
+}
