38e45e8391dd21d461672f64c59ad8135a0f90d0
Tom Six <tom.six@student.odisee.be>
2020-04-23 14:10:10 +0200

    added extra html attributes to the text-field component

--- resources/views/text-field.blade.php
+++ resources/views/text-field.blade.php
@@ -1,4 +1,20 @@
-<div>
-    <input type="text" />
-    {{ $name }}
+<div {{ $attributes->merge(['class' => 'form-group']) }}>
+    @isset($label)
+        <label for="{{ $name }}">{{ $label }}</label>
+    @endisset
+    <input
+        autocomplete="off"
+        type="{{ $type }}"
+        name="{{ $name }}"
+        id="{{ $name }}"
+        class="form-control @error($name) is-invalid @enderror"
+        placeholder="{{ $placeholder }}"
+        value="{{ old($name, $value) }}"
+        {{ $required }}
+    >
+    @error($name)
+        <div class="invalid-feedback">
+            {{ $message }}
+        </div>
+    @enderror
 </div>
--- src/View/Components/TextField.php
+++ src/View/Components/TextField.php
@@ -8,11 +8,56 @@ use Illuminate\View\Component;
 
 class TextField extends Component
 {
+    /**
+     * The input name
+     *
+     * @var string $name
+     */
     public string $name;
 
-    public function __construct(string $name)
+    /**
+     * The label text. There will no label rendered if it isn't provided
+     *
+     * @var string|null $label
+     */
+    public ?string $label;
+
+    /**
+     * The text for the placeholder
+     *
+     * @var string $placeholder
+     */
+    public string $placeholder;
+
+    /**
+     * Set the input as required
+     *
+     * @var string $required
+     */
+    public string $required;
+
+    /**
+     * Define a default value
+     *
+     * @var mixed $value
+     */
+    public $value;
+
+    /**
+     * Set the type of the input
+     *
+     * @var string $type
+     */
+    public string $type;
+
+    public function __construct(string $name, ?string $label = null, ?string $placeholder = null, bool $required = false, $value = null, string $type = null)
     {
         $this->name = $name;
+        $this->label = $label;
+        $this->placeholder = $placeholder ?? '';
+        $this->required = $required ? 'required' : '';
+        $this->value = $value ?? '';
+        $this->type = $type ?? 'text';
     }
 
     public function render()
