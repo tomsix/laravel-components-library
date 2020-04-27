6df29d8b6b7510135027ed7fbb9e0601df740de9
Tom Six <tom.six@student.odisee.be>
2020-04-27 09:36:21 +0200

    added a abstract class for all form-groups

---
+++ src/View/Components/FromGroup.php
@@ -0,0 +1,62 @@
+<?php
+
+
+namespace TomSix\Components\View\Components;
+
+
+use Illuminate\View\Component;
+
+abstract class FromGroup extends Component
+{
+    /**
+     * Specifies the name
+     *
+     * @var string $name
+     */
+    public string $name;
+
+    /**
+     * The label text. There will no label rendered if it isn't provided
+     *
+     * @var string|null $label
+     */
+    public ?string $label;
+
+    /**
+     * Define a default value
+     *
+     * @var mixed $value
+     */
+    public $value;
+
+    /**
+     * Specifies the disabled attribute
+     *
+     * @var string $disabled
+     */
+    public string $disabled;
+
+    /**
+     * Specifies the readonly attribute
+     *
+     * @var string $readonly
+     */
+    public string $readonly;
+
+    /**
+     * InputField constructor.
+     * @param string $name
+     * @param string|null $label
+     * @param bool $disabled
+     * @param bool $readonly
+     * @param mixed|null $value
+     */
+    public function __construct(string $name, ?string $label = null, bool $disabled = false, bool $readonly = false, $value = null)
+    {
+        $this->name = $name;
+        $this->label = $label;
+        $this->disabled = $disabled ? 'disabled' : '';
+        $this->readonly = $readonly ? 'readonly' : '';
+        $this->value = $value ?? '';
+    }
+}
--- src/View/Components/InputField.php
+++ src/View/Components/InputField.php
@@ -4,37 +4,14 @@
 namespace TomSix\Components\View\Components;
 
 
-use Illuminate\View\Component;
-
-abstract class InputField extends Component
+abstract class InputField extends FromGroup
 {
-    /**
-     * Specifies the name
-     *
-     * @var string $name
-     */
-    public string $name;
-
-    /**
-     * The label text. There will no label rendered if it isn't provided
-     *
-     * @var string|null $label
-     */
-    public ?string $label;
-
     /**
      * Specifies the placeholder
      *
      * @var string $placeholder
      */
     public string $placeholder;
-
-    /**
-     * Define a default value
-     *
-     * @var mixed $value
-     */
-    public $value;
 
     /**
      * Set the type of the input
@@ -50,19 +27,6 @@ abstract class InputField extends Component
      */
     public string $required;
 
-    /**
-     * Specifies the disabled attribute
-     *
-     * @var string $disabled
-     */
-    public string $disabled;
-
-    /**
-     * Specifies the readonly attribute
-     *
-     * @var string $readonly
-     */
-    public string $readonly;
 
     /**
      * InputField constructor.
@@ -77,13 +41,10 @@ abstract class InputField extends Component
      */
     public function __construct(string $name, ?string $label = null, ?string $placeholder = null, ?string $type = null, bool $required = false, bool $disabled = false, bool $readonly = false, $value = null)
     {
-        $this->name = $name;
-        $this->label = $label;
+        parent::__construct($name, $label, $disabled, $readonly, $value);
+
         $this->placeholder = $placeholder ?? '';
         $this->required = $required ? 'required' : '';
-        $this->disabled = $disabled ? 'disabled' : '';
-        $this->readonly = $readonly ? 'readonly' : '';
-        $this->value = $value ?? '';
         $this->type = $type ?? 'text';
     }
 }
