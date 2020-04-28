ed07be44997f123d97af16e82fa7c1b5475b69c8
Tom Six <tom.six@student.odisee.be>
2020-04-28 17:07:17 +0200

    implemented a test that checks if all components can be found

---
+++ tests/Unit/ComponentsTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace TomSix\Components\Test\Unit;
+
+use TomSix\Components\Test\TestCase;
+
+class ComponentsTest extends TestCase
+{
+    /** @test */
+    public function it_find_all_components()
+    {
+        $this->blade->compileString('<x-form-input  />');
+        $this->blade->compileString('<x-form-select  />');
+        $this->blade->compileString('<x-form-model-select  />');
+        $this->blade->compileString('<x-form-textarea  />');
+        $this->blade->compileString('<x-form-checkboxes  />');
+        $this->blade->compileString('<x-form-checkbox  />');
+        $this->blade->compileString('<x-form-errors  />');
+        $this->blade->compileString('<x-form-file  />');
+
+        $this->assertTrue(true);
+    }
+}
