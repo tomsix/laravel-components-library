e0c92bb0738977e6cebd20a6f6ab768009396a77
Tom Six <tom.six@student.odisee.be>
2020-04-29 10:49:33 +0200

    updated the tests with the input-group and error component

--- tests/Unit/ComponentsTest.php
+++ tests/Unit/ComponentsTest.php
@@ -10,12 +10,14 @@ class ComponentsTest extends TestCase
     public function it_find_all_components()
     {
         $this->blade->compileString('<x-form-input  />');
+        $this->blade->compileString('<x-form-input-group  />');
         $this->blade->compileString('<x-form-select  />');
         $this->blade->compileString('<x-form-model-select  />');
         $this->blade->compileString('<x-form-textarea  />');
         $this->blade->compileString('<x-form-checkboxes  />');
         $this->blade->compileString('<x-form-checkbox  />');
         $this->blade->compileString('<x-form-errors  />');
+        $this->blade->compileString('<x-form-error  />');
         $this->blade->compileString('<x-form-file  />');
 
         $this->assertTrue(true);
