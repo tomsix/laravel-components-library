8e03bad1ad6819aeef3eda3b3c4a2b639a35c2a4
tomsix <tom@webatvantage.be>
2020-10-15 13:56:00 +0200

    UPGRADE: improved the test that's checks if the components are loaded

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,7 +3,6 @@
 namespace TomSix\Components\Test;
 
 use Illuminate\Foundation\Testing\WithFaker;
-use Illuminate\View\Compilers\BladeCompiler;
 use Orchestra\Testbench\TestCase as Orchestra;
 use TomSix\Components\LibraryServiceProvider;
 
@@ -11,13 +10,9 @@ class TestCase extends Orchestra
 {
     use WithFaker;
 
-    protected BladeCompiler $blade;
-
     protected function setUp(): void
     {
         parent::setUp();
-
-        $this->blade = app('blade.compiler');
     }
 
     protected function getPackageProviders($app)
