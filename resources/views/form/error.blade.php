cad05fd425f2cf511d6475cc7a38d23daa6bfc74
tomsix <tom@webatvantage.be>
2021-09-03 09:53:50 +0200

    combine attributes on error component

--- resources/views/form/error.blade.php
+++ resources/views/form/error.blade.php
@@ -1,7 +1,5 @@
 @if(config('library.inline_errors'))
     @error($name)
-        <div class="{{ config('library.css.error.inline.div') }}">
-            {{ $message }}
-        </div>
+        <div {{ $attributes->class[config('library.css.error.inline.div')] }}>{{ $message }}</div>
     @enderror
 @endif
