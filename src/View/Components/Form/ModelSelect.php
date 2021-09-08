630a7290a95e1d7601141efa4a17dfb497088a44
tomsix <tom@webatvantage.be>
2021-09-08 16:08:43 +0200

    Make the model collection type flexible

--- src/View/Components/Form/ModelSelect.php
+++ src/View/Components/Form/ModelSelect.php
@@ -2,8 +2,9 @@
 
 namespace TomSix\Components\View\Components\Form;
 
-use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Collection as EloquentCollection;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Collection;
 
 class ModelSelect extends Select
 {
@@ -11,7 +12,7 @@ class ModelSelect extends Select
      * Create a new component instance.
      *
      * @param string                $name
-     * @param Collection|null       $models
+     * @param Collection|EloquentCollection|null       $models
      * @param string|null           $label
      * @param Model|string|int|null $value
      * @param string|null           $prepend
@@ -19,7 +20,7 @@ class ModelSelect extends Select
      * @param string|null           $keyAttribute
      * @param string|null           $valueAttribute
      */
-    public function __construct(string $name, ?Collection $models = null, ?string $label = null, $value = null, ?string $prepend = null, ?string $append = null, string $keyAttribute = null, string $valueAttribute = null)
+    public function __construct(string $name, $models = null, ?string $label = null, $value = null, ?string $prepend = null, ?string $append = null, string $keyAttribute = null, string $valueAttribute = null)
     {
         $keyAttribute = $keyAttribute ?: config('library.model_select.key_attribute');
         $valueAttribute = $valueAttribute ?: config('library.model_select.value_attribute');
